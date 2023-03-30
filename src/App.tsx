8e58f8dead25ba6ac96b2a305a6cb6f54d7c904f
ilia-io <likeapu@gmail.com>
2023-03-30 16:17:42 +0300

    feat: catalog page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import './app.scss';
-import BreadCrumbs from './components/BreadCrumbs';
-import Catalog from './components/Catalog';
+import CatalogPage from './pages/CatalogPage';
 
 function App() {
   return (
     <>
-      <Header />
-      <main className="main">
-        <BreadCrumbs />
-        <Catalog />
-      </main>
-      <Footer />
+      <CatalogPage />
     </>
   );
 }
--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import BreadCrumbs from '../components/BreadCrumbs';
+import Catalog from '../components/Catalog';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 type Props = {}
 
 const CatalogPage = (props: Props) => {
   return (
-    <div>CatalogPage</div>
-  )
+    <>
+      <Header />
+      <main className="main">
+        <BreadCrumbs />
+        <Catalog />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default CatalogPage
