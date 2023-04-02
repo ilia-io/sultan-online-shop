c2691075d21785d9d901620318fb173835c06194
ilia-io <likeapu@gmail.com>
2023-04-02 21:22:09 +0300

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './app.scss';
+import 'app.scss';
 import CatalogPage from './pages/CatalogPage';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
