797c901d8f7756b54d04a3d10a0164f712e3ad49
ilia-io <likeapu@gmail.com>
2023-04-12 19:08:36 +0300

    refactor: selection by params component

---
+++ src/components/SelectionByParams.tsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react'
+import { useAppDispatch } from '../app/hooks';
+import { setPriceFilterMax, setPriceFilterMin } from '../app/reducers/filterSlice';
+import SearchForm from './SearchForm';
+import ManufacturersList from './ManufacturersList';
+
+type Props = {}
+
+const SelectionByParams = (props: Props) => {
+  const [inputPriceMin, setInputPriceMin] = useState('0');
+  const [inputPriceMax, setInputPriceMax] = useState('10000');
+
+   const dispatch = useAppDispatch();
+
+  function handlePriceMin(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputPriceMin(e.target.value);
+    dispatch(setPriceFilterMin(e.target.value));
+  }
+
+  function handlePriceMax(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputPriceMax(e.target.value);
+    dispatch(setPriceFilterMax(e.target.value));
+  }
+  return (
+    
+      <>
+        <div className="catalog__price-filter-box">
+          <h2 className="catalog__price-filter-title">
+            Цена <span className="catalog__price-filter-currency">₸</span>
+          </h2>
+          <div className="catalog__price-filter-inputs">
+            <label htmlFor="priceMin"></label>
+            <input
+              id="priceMin"
+              type="number"
+              className="catalog__price-filter-min"
+              placeholder="0"
+              value={inputPriceMin}
+              onChange={handlePriceMin}
+            />
+            <p className="catalog__price-filter-divider">-</p>
+            <label htmlFor="priceMax"></label>
+            <input
+              id="priceMax"
+              type="number"
+              className="catalog__price-filter-max"
+              placeholder="10 000"
+              value={inputPriceMax}
+              onChange={handlePriceMax}
+            />
+          </div>
+        </div>
+        <div className="catalog__manufacturer-filter-box">
+          <h2 className="catalog__manufacturer-filter-title">Производитель</h2>
+          <SearchForm classPrefix="catalog" />
+          <ManufacturersList />
+
+          <div className="catalog__manufacturer-filter-divider"></div>
+        </div>
+      </>
+    
+  );
+}
+
+export default SelectionByParams
--- src/components/SideFilters.tsx
+++ src/components/SideFilters.tsx
@@ -1,74 +1,34 @@
 import React, { useState } from 'react';
-import SearchForm from './SearchForm';
-import ManufacturersList from './ManufacturersList';
-import {
-  filterSelector,
-  setActiveCaterogy,
-  setPriceFilterMax,
-  setPriceFilterMin,
-} from '../app/reducers/filterSlice';
+import { filterSelector, setActiveCaterogy } from '../app/reducers/filterSlice';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
+import SelectionByParams from './SelectionByParams';
 
 type Props = {};
 
 const SideFilters = (props: Props) => {
-  const [inputPriceMin, setInputPriceMin] = useState('0');
-  const [inputPriceMax, setInputPriceMax] = useState('10000');
-
+  const [showParams, setShowParams] = useState(true);
   const { activeCategory, categories } = useAppSelector(filterSelector);
+
   const dispatch = useAppDispatch();
-
-  function handlePriceMin(e: React.ChangeEvent<HTMLInputElement>) {
-    setInputPriceMin(e.target.value);
-    dispatch(setPriceFilterMin(e.target.value));
-  }
-
-  function handlePriceMax(e: React.ChangeEvent<HTMLInputElement>) {
-    setInputPriceMax(e.target.value);
-    dispatch(setPriceFilterMax(e.target.value));
-  }
 
   function handleClickCategory(categorie: string) {
     dispatch(setActiveCaterogy(categorie));
   }
   return (
     <section className="catalog__side-filters">
-      <h2 className="catalog__side-filters-title">ПОДБОР ПО ПАРАМЕТРАМ</h2>
-      <div className="catalog__mobile-filter-wrapper">
-        <div className="catalog__price-filter-box">
-          <h2 className="catalog__price-filter-title">
-            Цена <span className="catalog__price-filter-currency">₸</span>
-          </h2>
-          <div className="catalog__price-filter-inputs">
-            <label htmlFor="priceMin"></label>
-            <input
-              id="priceMin"
-              type="number"
-              className="catalog__price-filter-min"
-              placeholder="0"
-              value={inputPriceMin}
-              onChange={handlePriceMin}
-            />
-            <p className="catalog__price-filter-divider">-</p>
-            <label htmlFor="priceMax"></label>
-            <input
-              id="priceMax"
-              type="number"
-              className="catalog__price-filter-max"
-              placeholder="10 000"
-              value={inputPriceMax}
-              onChange={handlePriceMax}
-            />
-          </div>
-        </div>
-        <div className="catalog__manufacturer-filter-box">
-          <h2 className="catalog__manufacturer-filter-title">Производитель</h2>
-          <SearchForm classPrefix="catalog" />
-          <ManufacturersList />
-
-          <div className="catalog__manufacturer-filter-divider"></div>
-        </div>
+      <h2
+        onClick={() => setShowParams(!showParams)}
+        className="catalog__side-filters-title"
+      >
+        ПОДБОР ПО ПАРАМЕТРАМ
+      </h2>
+      <div
+        hidden={showParams ? false : true}
+        className="catalog__mobile-filter-wrapper"
+      >
+        <SelectionByParams />
       </div>
+
       <div className="catalog__side-filters-categories-box">
         <ul className="catalog__side-filters-categories-list">
           {categories.map((categorie) => (
