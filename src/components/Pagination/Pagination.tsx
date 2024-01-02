51cd5bec00e35d1aa424c2ea5f43770ab705c2be
ilia-io <likeapu@gmail.com>
2024-01-02 17:38:24 +0300

    fix: styles

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -9,12 +9,7 @@ type Props = {
   currentPage: number;
 };
 
-const Pagination = ({
-  productsPerPage,
-  totalProducts,
-  paginate,
-  currentPage,
-}: Props) => {
+const Pagination = ({ productsPerPage, totalProducts, paginate, currentPage }: Props) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
@@ -22,52 +17,56 @@ const Pagination = ({
   }
 
   return (
-    <nav className="pagination">
-      <img
-        src={leftIcon}
-        alt="arrow pointing left"
-        className="pagination__icon"
-        onClick={() => {
-          paginate(currentPage === 1 ? 1 : currentPage - 1);
-        }}
-      />
-      <ul className="pagination__list">
-        {pageNumbers.map((number, index) => (
-          <li
-            key={number}
-            className={
-              currentPage === index + 1
-                ? `pagination__item_active pagination__item`
-                : 'pagination__item'
-            }
-          >
-            <button
-              className={
-                currentPage === index + 1
-                  ? `pagination__btn_active pagination__btn`
-                  : 'pagination__btn'
-              }
-              type="button"
-              onClick={() => {
-                paginate(number);
-              }}
-            >
-              {number}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <img
-        src={rightIcon}
-        alt="arrow pointing right"
-        className="pagination__icon"
-        onClick={() => {
-          paginate(
-            currentPage < pageNumbers.length ? currentPage + 1 : currentPage
-          );
-        }}
-      />
-    </nav>
+    <>
+      {pageNumbers.length > 0 && (
+        <nav className="pagination">
+          <img
+            src={leftIcon}
+            alt="arrow pointing left"
+            className="pagination__icon"
+            onClick={() => {
+              paginate(currentPage === 1 ? 1 : currentPage - 1);
+            }}
+          />
+
+          <ul className="pagination__list">
+            {pageNumbers.map((number, index) => (
+              <li
+                key={number}
+                className={
+                  currentPage === index + 1
+                    ? `pagination__item_active pagination__item`
+                    : 'pagination__item'
+                }
+              >
+                <button
+                  className={
+                    currentPage === index + 1
+                      ? `pagination__btn_active pagination__btn`
+                      : 'pagination__btn'
+                  }
+                  type="button"
+                  onClick={() => {
+                    paginate(number);
+                  }}
+                >
+                  {number}
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <img
+            src={rightIcon}
+            alt="arrow pointing right"
+            className="pagination__icon"
+            onClick={() => {
+              paginate(currentPage < pageNumbers.length ? currentPage + 1 : currentPage);
+            }}
+          />
+        </nav>
+      )}
+    </>
   );
 };
 
